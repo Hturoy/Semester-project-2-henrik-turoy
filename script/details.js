a40146c21515bde74e30056edba30617daed4d9c
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-12-07 14:33:19 +0100

    Product details
    added product details page and function. now you can click any product and be brought to a page for that specific item

---
+++ script/details.js
@@ -0,0 +1,36 @@
+import { apiUrl } from "./utils/api.js"
+import { drawCard } from "./utils/drawList.js"
+
+const getUrl = document.location.search
+const para = new URLSearchParams(getUrl)
+const id = para.get('id')
+const url = apiUrl + `products/${id}`
+
+const container = document.querySelector('.details-container')
+
+
+console.log(url)
+
+let productList = []
+
+async function fetchApi () {
+    const response = await fetch (url);
+    const json = await response.json();
+
+    productList.push(json)
+    drawCards()
+}
+
+
+
+function drawCards () {
+    container.innerHTML=''
+    productList.forEach(product => {
+       
+            container.innerHTML += drawCard(product)
+        
+    });
+}
+
+
+fetchApi()
--- script/utils/drawList.js
+++ script/utils/drawList.js
@@ -3,10 +3,10 @@ export function drawCard (i){
 
     return `
         <div class='product-card'>
-            <img src='${image_url}' alt='${title}'>
-            <h3>${title}</h3>
+        <a href="productDetails.html?id=${id}"><img src='${image_url}' alt='${title}'>
+            <h3>${title}</h3></a>
             <div class='inner-product-container'>
-            <p>${price}$</p>
+            <a href="productDetails.html?id=${id}"><p>${price}$</p></a>
             <img id='${id}' src="/media/icons/cart.svg" alt="cart icon">
             </div>
         </div>
