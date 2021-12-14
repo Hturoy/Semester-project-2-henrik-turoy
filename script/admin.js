05569075744fafd7a95a0ddfdf3a580ad8e6f9e4
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-12-14 15:32:06 +0100

    login + admin page
    added login function, and started the admin page.

---
+++ script/admin.js
@@ -0,0 +1,22 @@
+const inputNameAdd = document.querySelector('#nameAdd');
+const inputPriceAdd = document.querySelector('#priceAdd');
+const inputDescAdd = document.querySelector('#descriptionAdd');
+const inputFeaturedAdd = document.querySelector('#featuredAdd');
+const inputImgAdd = document.querySelector('#imgAdd');
+const addBtn = document.querySelector('#add');
+
+
+const inputNameEdit = document.querySelector('#nameEdit');
+const inputPriceEdit = document.querySelector('#priceEdit');
+const inputDescEdit = document.querySelector('#descriptionEdit');
+const inputFeaturedEdit = document.querySelector('#featuredEdit');
+const inputImgEdit = document.querySelector('#imgEdit');
+const editBtn = document.querySelector('#edit');
+
+addBtn.addEventListener('click', () =>{
+    addItem()
+});
+
+function addItem () {
+    console.log('boiii')
+}
---
+++ script/login.js
@@ -0,0 +1,60 @@
+import { displayMsg } from './utils/displayMsg.js'
+import { saveToken, saveUser } from './utils/loginStorage.js'
+import { apiUrl } from './utils/api.js'
+
+const form = document.querySelector(".loginForm");
+const password = document.querySelector("#password");
+const username = document.querySelector("#username");
+const msg = document.querySelector(".msgContainer");
+
+form.addEventListener('submit', submitForm)
+
+function submitForm(event) {
+    event.preventDefault();
+
+    msg.innerHTML=''
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(usernameValue.length === 0 || passwordValue.length === 0) {
+        displayMsg('warning', 'invalid values', '.msgContainer')
+    }
+    
+
+    doLogin(usernameValue, passwordValue)
+}
+
+
+async function doLogin(username, password) {
+    const url = apiUrl + 'auth/local';
+    const data = JSON.stringify({ identifier: username, password: password });
+
+    const options = {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-type': 'application/json'
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+
+        if(json.user) {
+            displayMsg('success', 'Successfully logged in', '.msgContainer')
+            saveToken(json.jwt);
+            saveUser(json.user)
+
+            window.location.href='/'
+        }
+
+        if(json.error) {
+            displayMsg('warning', 'Invalid login details', '.msgContainer')
+        }
+
+        console.log(json);
+    }catch(error) {
+        console.log(error)
+    }
+}
